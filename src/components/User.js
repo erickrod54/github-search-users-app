c725390f2f09ce89255a4124e809463d6cc6f779
Erick Rodriguez <erickrod54@gmail.com>
2022-06-12 20:01:27 -0400

    Github-search-users app version 3 - User js file - Features: Importing and Placing Card Component, Importing and Placing Followers Component

--- src/components/User.js
+++ src/components/User.js
@@ -2,8 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 import Card from './Card';
 import Followers from './Followers';
+/**Github-search-users app version 3 - 'User' js file - 
+ * Features:
+ * 
+ *      --> Importing and Placing 'Card' Component.
+ * 
+ *      --> Importing and Placing 'Followers' Component.
+ * 
+ * 
+ * Notes: This component is going to set styles for both 
+ * Components, 'Card' Component -will have content for 
+ * the user-, and 'Followers' Component -will have 
+ * content for followers-
+ *  */
+
 const User = () => {
-  return <h2>user component</h2>;
+  return(
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <Card />
+        <Followers />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
