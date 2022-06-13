50c6c12717f7c27547ff4193a89924e5a9deb9d2
Erick Rodriguez <erickrod54@gmail.com>
2022-06-13 09:19:55 -0400

    Github-search-app version 4 - Respos js file - Features: Destructuring repos on GithubContext, Importing and Placing the ExampleChart in order to start working with charts, Wrapping with Wrapper Style Component the ExampleChart to style it

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -2,8 +2,38 @@ import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+
+/**Github-search-users app version 4 - 'Repos' js 
+ * file - Features:
+ * 
+ *      --> Destructuring 'repos' on 'GithubContext'.
+ * 
+ *      --> Importing and Placing the 'ExampleChart'
+ *          in order to start working on charts.
+ * 
+ *      --> Wrapping with 'Wrapper' Style Component
+ *          the 'ExampleChart' to style it.
+ * 
+ * Notes: This Component contain all the props 
+ * related with the repos. 
+ * 
+ * So first i test the data, then i start to set
+ * 'ExampleChart' to display the 'repos' data previously
+ * tested  
+ * */
 const Repos = () => {
-  return <h2>repos component</h2>;
+  
+  const { repos } = React.useContext(GithubContext)
+  
+  console.log('this is provided on Repos ==>',repos )
+
+  return(
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <ExampleChart />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
