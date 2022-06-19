159fcb05a36ccc126a28b8348002818ba9f86cbb
Erick Rodriguez <erickrod54@gmail.com>
2022-06-18 21:45:31 -0400

    Github-search-users app verion 14 - Repos js file - Features: Placing Column3D Component, Placing Bar3D Component, Building functionality for Column3D, Building functionality for Bar3D

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -3,15 +3,33 @@ import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 
-/**Github-search-users app version 13 - 'Repos' js 
+/**Github-search-users app version 14 - 'Repos' js 
  * file - Features:
  * 
- *      --> Calculationg 'Start Per Langueage' and 
- *          render them on 'Doughnut2D' Component. 
+ *      --> Placing 'Column3D' Component.
  * 
- * Notes: the code in the 'Wrapper' is related with the 
- * code in the 'Pie3D' chart about the 'relative' width. 
+ *      --> Placing 'Bar3D' Component.
  * 
+ *      --> Building functionality for 'Column3D'.
+ * 
+ *      --> Building functionality for 'Bar3D'.  
+ * 
+ * Notes: for now this components has been placed with
+ * the dummy data - chartData - 
+ * 
+ * The functionality for 'Column3D' first i build the
+ * 'stars' object then having this object with the props
+ * 'label' and 'value' same props that 'Column3D' needs
+ * i use 'Object' method to make 'starsArray' and display
+ * it on the 'Column3D' graph.
+ * 
+ * The functionality for 'Bar3D' is created based on 
+ * 'Column3D' functionality
+ * 
+ * in this version the 'chartData' dummy data is no 
+ * longer needed because this data was only to provide
+ * a reference to create 'Column3D' and 'Bar3D' with
+ * props base on 'label' and 'value'
  * */
 const Repos = () => {
   
@@ -54,6 +72,37 @@ const Repos = () => {
     return b.value - a.value
   }).slice(0, 5);
 
+  /** stars for 'Column3D' and forks  for 'Bar3D' 
+   * functionality */
+
+  /**'total' represent the acummulative result and 
+   * item every 'item'*/
+  let { stars, forks } = repos.reduce((total, item) => {
+    const { stargazers_count, name, forks } = item;
+
+    /**here i dynamicly create the object */
+    total.stars[stargazers_count] = {label:name, value: stargazers_count}
+    
+    /**here i dynamicly create the object */
+    total.forks[forks] = { label: name, value: forks}
+    return total;
+  }, {
+    stars:{}, forks:{}
+  })
+
+  console.log('The stars object returned for Column3D (object with label and value props) ==>',stars)
+
+  /**here i turned the 'stars' object into an Array and
+   * reordered using 'reverse' to get the values 
+   * top-down*/
+  const starsArray = Object.values(stars)
+  .slice(-5).reverse()
+
+  console.log('The stars object turned into an array ==>', starsArray)
+
+  /**here i make the same with "forks" */
+  const forksArray = Object.values(forks)
+  .slice(-5).reverse()
 
   /**Now i add the 'stars' calculation here */
   /**the difference is that i map and overwrite
@@ -74,21 +123,7 @@ const Repos = () => {
    * languages from all the repos */
   console.log('reduce resulting total of every language ==> ',Languages)
 
-  /**here i modifify the data */
-  const chartData = [
-  {
-    "label": "HTML",
-    "value": "13"
-  },
-  {
-    "label": "CSS",
-    "value": "100"
-  },
-  {
-    "label": "javaScript",
-    "value": "80"
-  },
-]
+ 
   
   //console.log('this is provided on Repos ==>',repos )
 
@@ -99,9 +134,17 @@ const Repos = () => {
         {/**here i drilled the array '' */}
           <Pie3D data={LanguagesArray}/>
           {/**this divs separates the charts*/}
-        <div></div>
+  
+        {/**here i place the 'Column3D' Component
+         * and pass the 'starsArray' data built*/}
+          <Column3D data={starsArray}/>
+
           {/**here i place the 'Doughnut2D' Component */}
           <Doughnut2D data={MostStarsArray}/>
+
+         {/**here i place the 'Bar3D' Component */}
+          <Bar3D data={forksArray}/>
+       
         <div></div>
       </Wrapper>
     </section>
