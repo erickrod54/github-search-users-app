6d914e4f3b97965c2a3b49e35d3a294d06970d55
Erick Rodriguez <erickrod54@gmail.com>
2022-06-19 01:10:10 -0400

    Uncommenting Navbar Component in order to get enought space and visualize the error feature

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,21 +3,24 @@ import { Info, Repos, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
 import { GithubContext } from '../context/context';
 
-/**Github-search-users app version 15 - 'Dashboard' 
+/**Github-search-users app version 16 - 'Dashboard' 
  * Component - Features:
  * 
- *      --> Uncommenting 'Search' Componnet in order to 
- *          work on it.
+ *      --> Uncommenting 'Navbar' Componnet in order to 
+ *          get enought space and visualize the error.
  * 
  * 
  * Notes: here we can see rendered the components and start
  * modeling them.
+ * 
+ * Navbar Component is uncommented on this version in order
+ * to get enought space and visualize the error
 */
 
 const Dashboard = () => {
   return (
     <main>
-      {/**<Navbar /> */}
+      <Navbar /> 
       <Search />
       <Info />
       <User />
