460e56a572bb0c832485f150cd10145c7ae5c132
Erick Rodriguez <erickrod54@gmail.com>
2022-06-27 02:59:41 -0400

    Github-search-users app version 19 - NavBar js file - Features: Destructuring the Auth0 props using the useAuth0 hook, Testing login feature, Testing logout feature, Testing changes on isAuthenticated, user, isLoading during the login process

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,35 @@ import React from 'react';
 import styled from 'styled-components';
 import { useAuth0 } from '@auth0/auth0-react';
 
+/**Github-search-users app version 19 - 'NavBar' js file - 
+ * Features:
+ * 
+ *      --> Destructuring the 'Auth0' props using the
+ *          'useAuth0' hook.     
+ * 
+ *      --> Testing 'login' feature.
+ *    
+ *      --> Testing 'logout' feature.
+ * 
+ *      --> Tesing changes on  'isAuthenticated', 'user', 
+ *        'isLoading' during the login process.
+ * 
+ * Notes: these props comes directly form the object 'Auth0'
+ * so i can use it to set all the authentication feature and
+ * this provider handles the detail of each prop :)
+ * */
+
 const Navbar = () => {
-  return <Wrapper>navbar component</Wrapper>;
+  
+  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } = useAuth0();
+
+  console.log('this is what it changes on auth process ==> ', { isAuthenticated, user, isLoading})
+
+  return (
+  <Wrapper>
+      <button onClick={loginWithRedirect}>login</button>
+      <button onClick={() => logout({ returnTo:window.location.origin})}>logout</button>
+  </Wrapper>);
 };
 
 const Wrapper = styled.nav`
