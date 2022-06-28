c771eaf6126abc4e643d77ff771074cdface17ab
Erick Rodriguez <erickrod54@gmail.com>
2022-06-28 00:57:04 -0400

    Github-search-users app version 20 - NavBar js file - Features: Building login feature, Building logout feature, Building isUser to create a conditional of the user existence and if is authenticated, Building a Welcome message with the name and picture of the user, Building a condition to show login or logout button dependeing on the conditions is is or not the user authenticated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,34 +2,56 @@ import React from 'react';
 import styled from 'styled-components';
 import { useAuth0 } from '@auth0/auth0-react';
 
-/**Github-search-users app version 19 - 'NavBar' js file - 
+/**Github-search-users app version 20 - 'NavBar' js file - 
  * Features:
  * 
- *      --> Destructuring the 'Auth0' props using the
- *          'useAuth0' hook.     
+ *      --> Building 'login' feature.
  * 
- *      --> Testing 'login' feature.
- *    
- *      --> Testing 'logout' feature.
+ *      --> Building 'logout' feature.
  * 
- *      --> Tesing changes on  'isAuthenticated', 'user', 
- *        'isLoading' during the login process.
+ *      --> Building 'isUser' to create 
+ *          a conditional of the user
+ *          existence and if is 
+ *          authenticated.
  * 
- * Notes: these props comes directly form the object 'Auth0'
- * so i can use it to set all the authentication feature and
- * this provider handles the detail of each prop :)
+ *      --> Building a Welcome message with
+ *          the name and picture of the user.
+ * 
+ *      --> Building a condition to show 'login'
+ *          or 'logout' button depending on the 
+ *          condition if is or not the user
+ *          authenticated.
+ * 
+ * Notes: During the authentication process i can see
+ * how the 'isloading', 'isAuthtenticated' and 'user'
+ * changes letting me know that is working properly
  * */
 
 const Navbar = () => {
   
   const { isAuthenticated, loginWithRedirect, logout, user, isLoading } = useAuth0();
 
+  /**building isUser in order to check if is authenticated
+   * an the user exists*/
+  const isUser = isAuthenticated && user;
+
   console.log('this is what it changes on auth process ==> ', { isAuthenticated, user, isLoading})
 
   return (
   <Wrapper>
-      <button onClick={loginWithRedirect}>login</button>
-      <button onClick={() => logout({ returnTo:window.location.origin})}>logout</button>
+      {/**checking user and showing picture */}
+      {isUser && user.picture && <img src={user.picture} alt={user.name}/>}
+
+      {/**checking user and showing name */}
+      {isUser && user.name && <h4>Welcome, <strong>{user.name.toUpperCase()}</strong></h4>}
+      { isUser ? 
+        (<button 
+          onClick={() => logout({ returnTo:window.location.origin})}
+          >logout</button>
+          ) :
+          (<button onClick={loginWithRedirect}>login</button>) 
+      }
+      
   </Wrapper>);
 };
 
