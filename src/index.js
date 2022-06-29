dec5e9b458bda66c5b735b3ffccecca4c646af8a
Erick Rodriguez <erickrod54@gmail.com>
2022-06-29 03:06:46 -0400

    Github-search-users app version 21 - index js file - Features: Setting cacheLocation prop on Auth0Provider object in order to make social media users persistent across the app

--- src/index.js
+++ src/index.js
@@ -6,33 +6,20 @@ import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-/**Github-search-users app version 19 - 'index' js file - 
+/**Github-search-users app version 21 - 'index' js file - 
  * Features:
  * 
- *      --> Wrapping the '<App>' with the 'AuthProvider'
- *          to set authentication features.
+ *      --> Setting 'cacheLocation' prop on 'Auth0Provide'
+ *        object in order to make 'social media users'
+ *        persistent across the app
  * 
- * Notes: the 'Auth0Provider' object needs this data:
- * 
- * domain ==>
- * dev-b4dh24tk.us.auth0.com
- *
- *  client ID ==>
- * 
- * ZbPdXZFtDkid2AhRhIN4pCGJ99d8EvLY
- * 
+ * Notes: the social media user data will be storage
+ * in the app to make the persistent.
+ *  
  * reference ==>
  * 
- * https://manage.auth0.com/dashboard/
- * 
- * Auth0 is an authentication provider and the way to set up
- * reference on the Docs:
- * 
- *    reference ==>
- * 
- *  https://auth0.com/docs/quickstart/spa/react#add-login-to-your-application
- *  
- * the data can be set as an .env variable
+ * https://auth0.com/docs/libraries/auth0-single-page-app-sdk
+ *
 */
 
 ReactDOM.render(
@@ -41,6 +28,7 @@ ReactDOM.render(
       domain="dev-b4dh24tk.us.auth0.com"
       clientId="ZbPdXZFtDkid2AhRhIN4pCGJ99d8EvLY"
       redirectUri={window.location.origin}
+      cacheLocation= 'localstorage'
      >
       <GithubProvider>
         <App />
