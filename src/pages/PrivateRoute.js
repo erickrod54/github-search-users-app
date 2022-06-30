0afbba83f99b7d1918bfd3be09c75076b85f5a24
Erick Rodriguez <erickrod54@gmail.com>
2022-06-30 01:09:32 -0400

    Github-search-users app version 22 - PrivateRoute js file - Features: Refactoring to implement reatc-router version 6, Implementation of Navigate instead of Redirect -this last component is no longer exists in react router version 6-

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
-/**Github-search-users app version 20 - 'PrivateRoute' js file - 
- * Features:
+/**Github-search-users app version 22 - 'PrivateRoute' 
+ * js file - Features:
  * 
- *      --> Issue with the authentication feature
- *          redirects me right to '/login' page
+ *      --> Refactoring to to implement 
+ *          'react-router version 6'.
  * 
- * Notes: i use the the 'google' and 'twitter' services
- * to get authenticated, also i try with different google
- * accounts, and try also sign in up a new user -the same
- * issue-
+ *      --> Implementation of 'Navigate' instead
+ *          of 'Redirect' -this component is no 
+ *          longer exists in react router version 6- 
  * 
+ * Notes: I have access to the children -Dashboard-
+ * and verifying that the user exists and 
+ * isAuthenticated i procced to return the children
+ * -Dashboard Component-
  * */
 
-const PrivateRoute = ({ children, ...rest }) => {
+const PrivateRoute = ({ children }) => {
   
   const { isAuthenticated, user } = useAuth0()
-  
-  const isUSer = isAuthenticated && user;
-  
-  return(
-    <>
-       <Route {...rest} render={ () => {
-        return( isUSer ? children : <Redirect to='/login'></Redirect>)
-       }} >
 
-       </Route>
+  const isUser = isAuthenticated && user;
 
-    </>
-  );
+  if (!isUser) {
+    return <Navigate to='/login'/>
+  }
+  
+  return children;
 };
 export default PrivateRoute;
